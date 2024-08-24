7d67be4d1ab7304e72d7701167f5d6baa2add0ec
fukicycle <lego.sato.4135@gmail.com>
2024-08-24 09:53:49 +0900

    Add firebase scheme

---
+++ kotli-english/Entities/Schemes/Progress.cs
@@ -0,0 +1,23 @@
+namespace kotli_english.Entities.Schemes;
+public sealed class Progress
+{
+    public Progress(
+        Guid wordId,
+        DateTime lastReviewed,
+        int correctResponses,
+        int incorrectResponses,
+        int masteryLevel
+    )
+    {
+        WordId = wordId;
+        LastReviewed = lastReviewed;
+        CorrectResponses = correctResponses;
+        IncorrectResponses = incorrectResponses;
+        MasteryLevel = masteryLevel;
+    }
+    public Guid WordId { get; }
+    public DateTime LastReviewed { get; }
+    public int CorrectResponses { get; }
+    public int IncorrectResponses { get; }
+    public int MasteryLevel { get; }
+}
