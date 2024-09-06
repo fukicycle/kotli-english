32ad4307d25fff74029238b20f9b26abb4403410
Fuki Sato <lego.sato.4135@gmail.com>
2024-09-06 13:27:19 +0900

    add welcome page

--- kotli-english/Pages/Home.razor.cs
+++ kotli-english/Pages/Home.razor.cs
@@ -1,10 +1,15 @@
 
 using kotli_english.Entities.Schemes;
+using Microsoft.AspNetCore.Components;
 
 namespace kotli_english.Pages;
 
 public partial class Home
 {
+    [Parameter]
+    public Guid UserId { get; set; } = Guid.Empty;
+
+    private bool _isNewUser = false;
     private Users? _user;
     protected override async Task OnInitializedAsync()
     {
@@ -13,13 +18,19 @@ public partial class Home
         {
             _user = await UserService.GetUserAsync();
         }
+        else if (UserId != Guid.Empty)
+        {
+            await UserService.SetUserIdFromQueryAsync(UserId);
+            _user = await UserService.GetUserAsync();
+        }
         else
         {
+            _isNewUser = true;
             //welcome page???
             //test add user.
             //TODO
-            await UserService.RegisterNewUserAsync();
-            NavigationManager.NavigateTo("", true);
+            // await UserService.RegisterNewUserAsync();
+            // NavigationManager.NavigateTo("", true);
         }
     }
 }
