c0289d5c7bf309bb5dbbe79a6412368377040926
fukicycle <lego.sato.4135@gmail.com>
2024-09-18 14:57:27 +0900

    resolve conflicts

--- kotli-english/Services/UserService.cs
+++ kotli-english/Services/UserService.cs
@@ -1,4 +1,6 @@
+using System.Text.RegularExpressions;
 using Blazored.LocalStorage;
+using Firebase.Database.Query;
 using kotli_english.Entities.Schemes;
 using kotli_english.Repositories.Interfaces;
 using kotli_english.Services.Interfaces;
@@ -10,10 +12,14 @@ public sealed class UserService : IUserService
     private const string USER_ID_STORAGE_KEY = "3de1e385-7892-4860-9531-87ba563d7497_user_id";
     private readonly IUserRepository _userRepository;
     private readonly ILocalStorageService _localStorageService;
-    public UserService(IUserRepository userRepository, ILocalStorageService localStorageService)
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<UserService> _logger;
+    public UserService(IUserRepository userRepository, ILocalStorageService localStorageService, IHttpClientFactory clientFactory, ILogger<UserService> logger)
     {
         _userRepository = userRepository;
         _localStorageService = localStorageService;
+        _httpClient = clientFactory.CreateClient("RandomUser");
+        _logger = logger;
     }
 
     public Guid UserId { get; private set; } = Guid.Empty;
@@ -36,10 +42,11 @@ public sealed class UserService : IUserService
     public async Task RegisterNewUserAsync()
     {
         Guid id = Guid.NewGuid();
+        string username = await GetRandomUserName();
         await _userRepository.AddUserAsync(
             new Users(
                 id.ToString(),
-                "New user",
+                username,
                 DateTime.Now,
                 "",
                 1,
@@ -47,6 +54,20 @@ public sealed class UserService : IUserService
             )
         );
         await _localStorageService.SetItemAsync(USER_ID_STORAGE_KEY, id);
+    }
+
+    private async Task<string> GetRandomUserName()
+    {
+        var response = await _httpClient.GetAsync("");
+        string content = await response.Content.ReadAsStringAsync();
+        string value = Regex.Match(content, "\"username\":\"*\"").Value;
+        string[] keyValues = value.Split(":");
+        if (keyValues.Count() != 2)
+        {
+            _logger.LogError("ユーザ名の生成に失敗しました。");
+            return "New user";
+        }
+        return keyValues[1].Replace("\"", "");
     }
 
     public async Task SetUserIdFromQueryAsync(Guid userId)
