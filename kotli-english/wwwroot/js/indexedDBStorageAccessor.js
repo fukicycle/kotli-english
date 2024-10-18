657061dcb9c88d8fd8cba260f5776f6c82613079
fukicycle <lego.sato.4135@gmail.com>
2024-10-18 09:51:28 +0900

    Add indexed db javascript and cs code

---
+++ kotli-english/wwwroot/js/indexedDBStorageAccessor.js
@@ -0,0 +1,36 @@
+﻿const CURRENT_VERSION = 1;
+const DATABASE_NAME = "kotli-english";
+export function initialize() {
+    const kotliEnglishIndexedDB = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
+    kotliEnglishIndexedDB.onupgradeneeded = function () {
+        const db = kotliEnglishIndexedDB.result;
+        db.createObjectStore("flashcard_settings", { keyPath: "id" });
+    }
+}
+
+export function set(collectionName, value) {
+    const kotliEnglishIndexedDB = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
+    kotliEnglishIndexedDB.onsuccess = function () {
+        const transaction = kotliEnglishIndexedDB.result.transaction(collectionName, "readwrite");
+        const collection = transaction.objectStore(collectionName);
+        collection.put(value);
+    }
+}
+
+export async function get(collectionName, id) {
+    const request = new Promise((resolve) => {
+        const kotliEnglishIndexedDB = indexedDB.open(DATABASE_NAME, CURRENT_VERSION);
+        kotliEnglishIndexedDB.onsuccess = function () {
+            const transaction = kotliEnglishIndexedDB.result.transaction(collectionName, "readonly");
+            const collection = transaction.objectStore(collectionName);
+            const result = collection.get(id);
+
+            result.onsuccess = function (e) {
+                resolve(result.result);
+            }
+        }
+    });
+
+    const result = await request;
+    return result;
+}
